d4f76591ebbd732b7edd6a3b9b194571ae14eea8
Aman Rawat <ar5941415@gmail.com>
2026-02-05 13:21:55 +0530

    removeing firebase config

--- BookStore/src/context/Firebase.jsx
+++ BookStore/src/context/Firebase.jsx
@@ -23,13 +23,13 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC_836m7i2G20mnhQWOAWnDuW0wdCWvubk",
-  authDomain: "book-store-29816.firebaseapp.com",
-  projectId: "book-store-29816",
-  storageBucket: "book-store-29816.firebasestorage.app",
-  messagingSenderId: "218015601985",
-  appId: "1:218015601985:web:be3020aedce804ab68fdc0",
-  measurementId: "G-M7ZJYZWLYT",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 //step1: Initialize Firebase app and get references to auth and database services
